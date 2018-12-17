ac0f65149c3b8c818dfbe3d23b07dfa1a6bcbadc
Max Keyvery <maxkeyvery@gmail.com>
2018-12-17 13:36:55 +0200

    Updates

---
+++ precache-manifest.175dae522afc576d9c544f000ec72a1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "409404b27521631a7918",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "409404b27521631a7918",
+    "url": "/contactmanager/static/js/main.409404b2.chunk.js"
+  },
+  {
+    "revision": "d206f8c89e3156f24a51",
+    "url": "/contactmanager/static/css/1.26321361.chunk.css"
+  },
+  {
+    "revision": "d206f8c89e3156f24a51",
+    "url": "/contactmanager/static/js/1.d206f8c8.chunk.js"
+  },
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "baa8eeb8148bdca3d65c3464488acc5b",
+    "url": "/contactmanager/index.html"
+  }
+];
